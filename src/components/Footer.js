453986598dd04f4dc8a40dedbfedfa8c555bc196
Parthiv Hirani <parthivhirani2903@gmail.com>
2025-06-29 14:26:49 +0530

    updated redirection urls to pass github CI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ const footer = () => {
 
                 <div className='phone'>
                     <h4><FaFile size={20} style={{color:'#fff', marginRight:'2rem'}}/>
-                    <a href='https://drive.google.com/drive/folders/14H61BkBwAnqFic2gux7aqrwhWMluLpRl' style={{color:'#1a64c5'}} target='_blank'>View Resume</a></h4>
+                    <a href='https://drive.google.com/drive/folders/14H61BkBwAnqFic2gux7aqrwhWMluLpRl' style={{color:'#1a64c5'}} target='_blank' rel='noreferrer'>View Resume</a></h4>
                 </div>
 
                 <div className='email'>
@@ -30,13 +30,13 @@ const footer = () => {
                 <h4>About Me</h4>
                 <p>Software Engineer. Coding Enthusiastic. Always ready to learn new technologies.</p>
                 <div className='social'>
-                <a href='https://www.linkedin.com/in/parthiv-hirani-61a6421b8' target='_blank'>
+                <a href='https://www.linkedin.com/in/parthiv-hirani-61a6421b8' target='_blank' rel='noreferrer'>
                 <FaLinkedin size={20} style={{color:'#fff', marginRight:'2rem'}}/></a>
-                <a href='https://twitter.com/parthivh1?t=ZTH1eXP108hwWPOLOwTACw&s=09' target='_blank'>
+                <a href='https://twitter.com/parthivh1?t=ZTH1eXP108hwWPOLOwTACw&s=09' target='_blank' rel='noreferrer'>
                 <FaTwitter size={20} style={{color:'#fff', marginRight:'2rem'}}/></a>
-                <a href='https://github.com/parthivhirani' target='_blank'>
+                <a href='https://github.com/parthivhirani' target='_blank' rel='noreferrer'>
                 <FaGithub size={20} style={{color:'#fff', marginRight:'2rem'}}/></a>
-                <a href='https://medium.com/@parthivhirani' target='_blank'>
+                <a href='https://medium.com/@parthivhirani' target='_blank' rel='noreferrer'>
                 <FaMediumM size={20} style={{color:'#fff', marginRight:'2rem'}}/></a>
                 </div>
             </div>
